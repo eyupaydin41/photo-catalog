407501119b9efee4fe1fcb232f560f2b2f81de3c
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-07-13 16:14:48 +0300

    Added EJS templating and multiple routes

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require(`express`);
 const path = require(`path`);
+const ejs = require(`ejs`);
 
 const app = express();
+
+app.set("view engine","ejs")
 
 app.use(express.static(`public`));
 
 const port = 3000;
 
 app.get(`/`, (req, res) => {
-        res.sendFile(path.resolve(__dirname, `temp/index.html`));
+    res.render(`index`);
 })
+app.get(`/about`, (req, res) => {
+    res.render(`about`);
+})
+app.get(`/add`, (req, res) => {
+    res.render(`add`);
+})
+
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı...`);
