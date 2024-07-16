5735be17d2e42cb3c938b37bbdeeb56db39b52a9
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-07-16 17:37:38 +0300

    - Set up MongoDB connection - Add dynamic photo addition and display

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 const express = require(`express`);
 const path = require(`path`);
 const ejs = require(`ejs`);
+const Photo = require(`./model/Photo`)
+const mongoose = require('mongoose').default;
 
 const app = express();
 
-app.set("view engine","ejs")
+mongoose.connect('mongodb://localhost/pcat-db')
+    .then(() => console.log("Başarıyla veritabanı ile bağlantı kuruldu."))
+    .catch((err) => console.log("Veritabanına bağlanırken hata oluştu: ", err));
 
+
+app.set("view engine","ejs");
 app.use(express.static(`public`));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 const port = 3000;
 
-app.get(`/`, (req, res) => {
-    res.render(`index`);
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı...`);
 })
+
+app.get(`/`, async (req, res) => {
+    const photos = await Photo.find({});
+    res.render(`index`, {
+        photos
+    });
+})
+
 app.get(`/about`, (req, res) => {
     res.render(`about`);
 })
+
 app.get(`/add`, (req, res) => {
     res.render(`add`);
 })
 
-
-app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı...`);
+app.post(`/photos`, async (req, res) => {
+    await Photo.create(req.body);
+    res.redirect(`/`);
 })
